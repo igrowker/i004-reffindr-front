aed1cb3881538b89fe3226e0037e21731bc89eed
Estrella Iveth Flores Lucas <estrellaflores550@gmail.com>
2024-11-16 01:12:59 -0600

    act

--- src/app/UI/components/index.ts
+++ src/app/UI/components/index.ts
@@ -1,7 +1,4 @@
 export * from './breadcrumbs/Breadcrumb'
 
-// src/app/UI/components/UserProfile/index.ts
-export { default } from './UserProfile';
 
-// src/app/UI/components/index.ts
-export { default as UserProfile } from './UserProfile'; // Aquí importas t
+export { default as UserProfile } from './UserProfile'; 

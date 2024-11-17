f574fa85c2566cdc7e6ef76d12bdf6fbb9193f5d
Jose A. Canto <jose.a.c.lopez@gmail.com>
2024-11-17 20:48:59 +0100

    Refactor LoginView to use Fieldset component and improve layout

--- src/app/features/Login/views/LoginView.tsx
+++ src/app/features/Login/views/LoginView.tsx
@@ -1,6 +1,8 @@
-import { Box, Button, Input, Link, Stack, Text } from '@chakra-ui/react'
+import { Box, Button, Fieldset, IconButton, Input, Link, Stack, Text } from '@chakra-ui/react'
 import { useState } from 'react'
-import { FaFacebook, FaGoogle } from 'react-icons/fa'
+import { FaFacebook, FaGoogle, FaTimes } from 'react-icons/fa'
+
+import { Field } from '@/components/ui/field'
 
 export const LoginView = () => {
   const [email, setEmail] = useState('')
@@ -13,72 +15,77 @@ export const LoginView = () => {
   }
 
   return (
-    <Box p={4} maxW='md' mx='auto' mt={10} bg='white' boxShadow='md'>
-      <Stack textAlign={'center'}>
-        <Text fontSize='4xl' fontWeight={'bold'} mb={4}>
-          Iniciar sesión
-        </Text>
-        <Text fontWeight={'medium'}>Completa los siguientes campos:</Text>
-      </Stack>
-
-      <Box mb={4}>
-        <Text mb={2}>Email*</Text>
-        <Input
-          type='email'
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder='Correo Electrónico'
-          borderColor='black'
-        />
-      </Box>
-
-      <Box mb={4}>
-        <Text mb={2}>Contraseña*</Text>
-        <Input
-          type='password'
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder='Contraseña'
-          borderColor='black'
-        />
-      </Box>
-
-      <Stack gap={4} mb={4}>
-        <Button w='full' borderColor='black' borderWidth='1px' bg={'#146EB4'} onClick={handleLogin}>
-          Iniciar Sesión
-        </Button>
-
-        <Button
-          colorScheme='red'
-          w='full'
-          bg='white'
-          color='black'
-          borderWidth='1px'
-          borderColor='black'
-          onClick={handleLogin}
-        >
-          <FaGoogle />
-          Iniciar sesión con Google
-        </Button>
-
-        <Button w='full' bg='white' color='black' borderWidth='1px' borderColor='black' onClick={handleLogin}>
-          <FaFacebook />
-          Iniciar sesión con Facebook
-        </Button>
-      </Stack>
-
-      <Stack direction='column' align='center' gap={3} mt={3}>
-        <Link href='#' textDecoration={'underline'}>
-          ¿Olvidaste tu contraseña?
-        </Link>
-
-        <Stack flexDirection={'row'}>
-          <Text>¿No tienes cuenta?</Text>
-          <Link fontWeight={'bold'} textDecoration={'underline'} href='#'>
-            Regístrate
-          </Link>
+    <Box minH='100vh' display='flex' alignItems='center' justifyContent='center' bg='gray.100'>
+      <Fieldset.Root paddingY={10} maxW='sm' mx='auto' mt={5} bg='white' boxShadow='md' paddingX={10}>
+        <IconButton bottom={8} left={8} alignSelf={'end'} size={'xs'}>
+          <FaTimes />
+        </IconButton>
+        <Stack textAlign={'center'} mt={-7}>
+          <Fieldset.Legend fontSize='4xl' fontWeight={'bold'} mb={4}>
+            Iniciar sesión
+          </Fieldset.Legend>
+          <Fieldset.HelperText fontWeight={'medium'}>Completa los siguientes campos:</Fieldset.HelperText>
         </Stack>
-      </Stack>
+
+        <Fieldset.Content>
+          <Field label='Email*' mb={4}>
+            <Input
+              type='email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder='Correo Electrónico'
+              borderColor='black'
+            />
+          </Field>
+
+          <Field label='Contraseña*' mb={4}>
+            <Input
+              type='password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder='Contraseña'
+              borderColor='black'
+            />
+          </Field>
+        </Fieldset.Content>
+
+        <Stack gap={4} mb={4}>
+          <Button type='submit' w='full' borderColor='black' borderWidth='1px' bg={'#146EB4'} onClick={handleLogin}>
+            Iniciar Sesión
+          </Button>
+
+          <Button
+            colorScheme='red'
+            w='full'
+            bg='white'
+            color='black'
+            borderWidth='1px'
+            borderColor='black'
+            onClick={handleLogin}
+          >
+            <FaGoogle />
+            Iniciar sesión con Google
+          </Button>
+
+          <Button w='full' bg='white' color='black' borderWidth='1px' borderColor='black' onClick={handleLogin}>
+            <FaFacebook />
+            Iniciar sesión con Facebook
+          </Button>
+        </Stack>
+
+        <Stack direction='column' align='center' gap={3} mt={3}>
+          <Link href='#' textDecoration={'underline'}>
+            ¿Olvidaste tu contraseña?
+          </Link>
+
+          <Stack flexDirection={'row'}>
+            <Text>¿No tienes cuenta?</Text>
+            <Link fontWeight={'bold'} textDecoration={'underline'} href='#'>
+              Regístrate
+            </Link>
+          </Stack>
+        </Stack>
+      </Fieldset.Root>
     </Box>
   )
 }

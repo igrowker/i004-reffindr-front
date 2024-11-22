becb8de5c60152b196fbb8d5cee38833257e671e
Estrella Iveth Flores Lucas <estrellaflores550@gmail.com>
2024-11-22 12:15:39 -0600

    Agregando archivos para la renderizacion de datos del propietario

--- src/app/UI/components/MenuAnnouncement/MenuAnnouncement.tsx
+++ src/app/UI/components/MenuAnnouncement/MenuAnnouncement.tsx
@@ -1,23 +1,39 @@
-import { Box, Flex, Text } from '@chakra-ui/react'
-import { useState } from 'react'
+import { Box, Flex, Text } from "@chakra-ui/react";
+import { useState } from "react";
+import Demo from "../ownerData/OwnerForm";
 
 export const MenuAnnouncement = () => {
-  const [activeTab, setActiveTab] = useState('Caracteristicas')
+  const [activeTab, setActiveTab] = useState("Caracteristicas");
 
-  const tabs = ['Detalles de propiedad', 'Caracteristicas', 'Datos del propietario', 'Confirmación']
+  const tabs = [
+    "Detalles de propiedad",
+    "Caracteristicas",
+    "Datos del propietario",
+    "Confirmación",
+  ];
 
   return (
-    <Flex justifyContent='start' mb={3} p={2}>
-      {tabs.map((tab) => (
-        <Box key={tab}>
-          <Text p={2}>{tab}</Text>
+    <>
+      {/* Aqui hago la navegacion eners las pestañas*/}
+      <Flex justifyContent="start" mb={3} p={2}>
+        {tabs.map((tab) => (
           <Box
-            borderColor={activeTab === tab ? 'blue.500' : 'black'}
-            borderBottom='2px solid'
-            color={activeTab === tab ? 'blue.500' : 'black'}
-          />
-        </Box>
-      ))}
-    </Flex>
-  )
-}
+            key={tab}
+            cursor="pointer" 
+            onClick={() => setActiveTab(tab)} 
+          >
+            <Text p={2}>{tab}</Text>
+            <Box
+              borderColor={activeTab === tab ? "blue.500" : "black"}
+              borderBottom="2px solid"
+              color={activeTab === tab ? "blue.500" : "black"}
+            />
+          </Box>
+        ))}
+      </Flex>
+
+      {/* Aqui estoy renderizando los datos del propietario*/}
+      {activeTab === "Datos del propietario" && <Demo />}
+    </>
+  );
+};

d1df98c7500ec199fb9ffe579baa466b70838176
joaquin feola <joacoarielfeola@gmail.com>
2024-11-23 20:33:13 -0300

    absolute import in file main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,17 @@
+import ' @/i18n'
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
 import { RouterProvider } from 'react-router-dom'
 
 import { Provider } from '@/components/ui/provider'
 import { router } from '@/router'
-
-import './i18n'
-import { router } from './router'
 
 const root = createRoot(document.getElementById('root')!)
 
 root.render(
   <React.StrictMode>
     <Provider forcedTheme='light'>
-      <RouterProvider router={router}  />
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 )

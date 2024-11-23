66601b17b7806d9cfb0e43ba5dbabc414ad8efe2
Jose A. Canto <jose.a.c.lopez@gmail.com>
2024-11-23 17:40:56 +0100

    improve error handling in useLogin hook and add timer for error messages

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -1,22 +1,32 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import { authLogin } from '@/services/authService'
 
 export const useLogin = () => {
-  const [errors, setErrors] = useState<string[] | null>(null)
+  const [errorsMessage, setErrorsMessage] = useState<string[] | null>(null)
 
   const login = async (email: string, password: string) => {
     const response = await authLogin(email, password)
 
     if (response.hasErrors) {
-      setErrors(response.errors)
+      setErrorsMessage(response.errors)
       console.log('Errors:', response.errors)
       return
     }
-    setErrors(null)
+    setErrorsMessage(null)
     sessionStorage.setItem('token', response.data?.token!)
     console.log('Token:', response.data?.token)
   }
 
-  return { login, errors }
+  useEffect(() => {
+    if (errorsMessage) {
+      const timer = setTimeout(() => {
+        setErrorsMessage(null)
+      }, 4000)
+
+      return () => clearTimeout(timer)
+    }
+  }, [errorsMessage])
+
+  return { login, errorsMessage }
 }

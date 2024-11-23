420d4655b319e2044a1cc2bd7344b8e371032f6a
joaquin feola <joacoarielfeola@gmail.com>
2024-11-23 20:27:21 -0300

    Adding response card

--- src/app/UI/components/profile/ProfileCard.tsx
+++ src/app/UI/components/profile/ProfileCard.tsx
@@ -18,15 +18,15 @@ interface ProvisionalUser {
 
 export const ProfileCard = ({ description, user }: Props) => {
   return (
-    <Flex maxW={'90%'} height={250} mx={'auto'} rounded='xs' shadow={'md'} gapX={4}>
-      <Image flexShrink={0} w={'1/4'} src={'/public/tamara-bellis-A3Gd2b-98_g-unsplash.jpg'} />
-      <Box py={2} overflow='auto'>
+    <Flex flexDirection={{base: "column", md: "row"}} maxW={'90%'} height={{base: "auto", md: "250px"}} mx={'auto'} rounded='xs' shadow={'md'} gapX={4}>
+      <Image  flexShrink={0} w={{base: "full", md: '1/4'}} h={{base: "300px", md: "250px"}} src={'/public/tamara-bellis-A3Gd2b-98_g-unsplash.jpg'} />
+      <Box pt={2} pb={4} px={4} overflow='auto'>
         <Heading py={2}>
           {user.firstName} {user.surname}
         </Heading>
         <Text>Propietaria de "xxxx"</Text>
         <Rating py={1} defaultValue={user.rating} allowHalf readOnly />
-        <Flex alignItems={'center'}>
+        <Flex my={2} alignItems={'center'}>
           <IoLocationOutline size={16} />
           <Text ml={4}>{user.city}, {user.province}</Text>
         </Flex>
